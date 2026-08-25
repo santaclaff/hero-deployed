e833ff9fa1524d1a747bb7e96f98fa1f246ff9a4
santaclaff <165549798+santaclaff@users.noreply.github.com>
2026-08-25 12:21:36 +0800

    Finalizations for wider E-rank testing

--- hero/www/js/plugins.js
+++ hero/www/js/plugins.js
@@ -138,7 +138,7 @@ var $plugins =
 {"name":"HERO_ShopGenerator","status":true,"description":"v1.00 Generates persistent random shops through Common Event 39.","parameters":{"Shop Variable":"40","Gacha Common Event":"39","Minimum Items":"3","Maximum Items":"4","Minimum Price":"60"}},
 {"name":"HERO_SwimmingEffect","status":true,"description":"[HERO] Visual swimming/submersion effect for events.","parameters":{"Visual Offset":"12","Drowning Amplitude":"3","Drowning Speed":"0.08"}},
 {"name":"ChampX_PlaytimeCorrection","status":false,"description":"Counts total playtime accurately regardless of framerate.","parameters":{}},
+{"name":"HERO_BattleStallMonitor","status":true,"description":"v1.10 On-screen diagnostic and recovery for stalled battles.","parameters":{}},
 {"name":"PATCHES","status":true,"description":"Fixes missing _summons array for old saves.","parameters":{}},
-{"name":"DEBUGGER","status":true,"description":"","parameters":{}},
-{"name":"HERO_BattleStallMonitor","status":true,"description":"v1.10 On-screen diagnostic and recovery for stalled battles.","parameters":{}}
+{"name":"DEBUGGER","status":true,"description":"","parameters":{}}
 ];
